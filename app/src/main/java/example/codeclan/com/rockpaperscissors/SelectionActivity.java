a4d9f3c2e018bb549c8da9e2d0653b206a9fa30f
Dominic <mrdjfraser@gmail.com>
2017-04-19 11:37:49 +0100

    rock button working

--- app/src/main/java/example/codeclan/com/rockpaperscissors/SelectionActivity.java
+++ app/src/main/java/example/codeclan/com/rockpaperscissors/SelectionActivity.java
@@ -3,13 +3,45 @@ package example.codeclan.com.rockpaperscissors;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SelectionActivity extends AppCompatActivity {
+
+    private TextView title_and_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
         Log.d(getClass().toString(), "onCreate for Selection");
+
+        title_and_result = (TextView) findViewById(R.id.title_and_result);
+    }
+
+    public void onRockClick(View text){
+        Log.d(getClass().toString(), "onRockClick clicked");
+
+        String selection = "Rock";
+
+        String result = GameLogic.WinChecker(selection,GameLogic.computerChoiceMaker());
+
+        title_and_result.setText(result);
+
     }
 }
+
+//
+//    public void onWordsEntered(View text){
+//        Log.d(getClass().toString(), "show uniq count button pressed");
+//
+//        String words = enteredWords.getText().toString();
+//
+//        Intent intent = new Intent(this,UniqWordsDisplayActivity.class);
+//
+//        intent.putExtra("words",words);
+//
+//        startActivity(intent);
+//    }
