6d218163e1915a57a2230f4f882ce64c678b7e3e
Dominic <mrdjfraser@gmail.com>
2017-04-19 15:47:59 +0100

    created GameResult class

---
+++ app/src/main/java/example/codeclan/com/rockpaperscissors/GameResult.java
@@ -0,0 +1,20 @@
+package example.codeclan.com.rockpaperscissors;
+
+/**
+ * Created by user on 19/04/2017.
+ */
+
+public class GameResult {
+
+    private String playerChoice;
+    private String computerChoice;
+    private String result;
+
+    public GameResult(String playerChoice, String computerChoice, String result){
+        this.playerChoice = playerChoice;
+        this.computerChoice = computerChoice;
+        this.result = result;
+    }
+
+
+}
