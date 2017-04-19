d8333af360d8297467be35bd6ba36bb294a625d4
Dominic <mrdjfraser@gmail.com>
2017-04-19 10:24:45 +0100

    first empty class created and testing git

---
+++ app/src/main/java/example/codeclan/com/rockpaperscissors/GameLogic.java
@@ -0,0 +1,12 @@
+package example.codeclan.com.rockpaperscissors;
+
+/**
+ * Created by user on 19/04/2017.
+ */
+
+public class GameLogic {
+
+    public static String WinChecker(String userChoice){
+        
+    }
+}
